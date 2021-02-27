6bf3f175df1fd7910dde2e3700e80f1aa223bc9a
gdg <2488041550@qq.com>
2021-02-27 16:01:27 +0800

    完成了登陆功能

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/login.vue'
+import Home from '../components/Home.vue'
+
+
+Vue.use(Router)
+
+// export default new Router({
+//   routes: [
+//     { path: '/', redirect: '/login' },
+//     { path: '/login', component: Login },
+//     { path: '/home', component: Home }
+//   ]
+// })
+const router = new Router({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    // 增加 home 规则
+    { path: '/home', component: Home }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //     next()  放行    next('/login')  强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+export default router
